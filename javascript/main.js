a850bd5525ba6de394cba3514a9bfacc9694b3fb
Diakite <adamdiakite22@gmail.com>
2018-11-16 18:41:50 +0100

    adding main.js to the repository

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+<script type ="text/javascript">
+   window.onload = selTimeout (
+       function() {
+           var r = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); 
+           if (r== true) {
+        window.location = https://www.youtube.com/watch?v=dQw4w9WgXcQ. ; 
+           } else { 
+            return; 
+        }
+}
+
+    , 10000);      
+</script>
